e970dfa2fa53855b83a186a1d46874d5ddb31dbf
arkadeep16 <paularka544@gmail.com>
2024-07-24 01:12:28 +0530

    first push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  function click() {
+    setCount(`Button clicked ${count++} times`);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>{count}</p>
+        <button onClick={click}>Click me</button>
     </div>
   )
 }
